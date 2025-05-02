01f470aec7c76e0c011c7b2e3c90ed162ae97133
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2025-05-02 22:45:21 +0800

    Initial commit

---
+++ src/main/java/example/springframeworkbible/SpringFrameworkBibleApplication.java
@@ -0,0 +1,13 @@
+package example.springframeworkbible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkBibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkBibleApplication.class, args);
+    }
+
+}
